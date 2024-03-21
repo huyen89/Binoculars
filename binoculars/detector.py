edead9f247cf08b5cff220d41e504f442f94c7be
Ha Huyen <76937269+huyen89@users.noreply.github.com>
2024-03-21 17:11:54 +0000

    codet5

--- binoculars/detector.py
+++ binoculars/detector.py
@@ -21,8 +21,8 @@ DEVICE_2 = "cuda:1" if torch.cuda.device_count() > 1 else DEVICE_1
 
 class Binoculars(object):
     def __init__(self,
-                 observer_name_or_path: str = "microsoft/codebert-base",
-                 performer_name_or_path: str = "microsoft/codebert-base-mlm",
+                 observer_name_or_path: str = "Salesforce/codet5-base",
+                 performer_name_or_path: str = "Salesforce/codet5p-220m",
                  use_bfloat16: bool = True,
                  max_token_observed: int = 512,
                  mode: str = "low-fpr",
@@ -35,15 +35,15 @@ class Binoculars(object):
                                                                    trust_remote_code=True,
                                                                    torch_dtype=torch.bfloat16 if use_bfloat16
                                                                    else torch.float32,
-                                                                   token=huggingface_config["TOKEN"]
-                                                                   )
+                                                                   token=huggingface_config["TOKEN"],
+                                                                   is_decoder=False).encoder
         self.performer_model = AutoModelForCausalLM.from_pretrained(performer_name_or_path,
                                                                     device_map={"": DEVICE_2},
                                                                     trust_remote_code=True,
                                                                     torch_dtype=torch.bfloat16 if use_bfloat16
                                                                     else torch.float32,
                                                                     token=huggingface_config["TOKEN"]
-                                                                    )
+                                                                    is_decoder=True).decoder
         self.observer_model.eval()
         self.performer_model.eval()
 
